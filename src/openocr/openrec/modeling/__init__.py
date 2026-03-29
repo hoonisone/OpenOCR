862e1a84bde4900d9df9a0790fbbf57b85143278
Hoonisone <hoonsine@gmail.com>
2026-03-29 13:27:37 +0000

    make module map in model component

--- src/openocr/openrec/modeling/__init__.py
+++ src/openocr/openrec/modeling/__init__.py
@@ -5,6 +5,10 @@ from .base_recognizer import BaseRecognizer
 __all__ = ['build_model']
 
 
+MODULES = {
+    "BaseRecognizer": BaseRecognizer
+}
+
 def build_model(config):
     config = copy.deepcopy(config)
     rec_model = BaseRecognizer(config)
